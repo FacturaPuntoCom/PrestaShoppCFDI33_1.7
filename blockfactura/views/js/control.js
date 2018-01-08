1a4788fca85a631cf36fe1c82ea835026680010a
jalil <jalitopi@gmail.com>
2018-01-08 11:00:21 -0600

    fix muestra errores

--- blockfactura/views/js/control.js
+++ blockfactura/views/js/control.js
@@ -313,7 +313,7 @@ function invoice(rfc, uid, order, method, num_cta){
         setTimeout(function(){
           swal({
             title: "¡Algo ocurrio!",
-            text: json.message,
+            text: json.message.message,
             type: 'warning',
             showConfirmButton: true
           });
